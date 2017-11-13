be2ac5a91812924509ce58cc731e9af8b56debe4
Butterknife <jurrewolters@gmail.com>
2017-11-13 22:14:33 +0100

    Add files via upload

---
+++ logintest.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import smtplib
+from lxml import html
+
+marks = []
+payload = {
+    "ctl00$ContentPlaceHolder1$Username$input": "10656",
+    "ctl00$ContentPlaceHolder1$Password$input": "--",
+}
+
+def CheckChange(tweet):
+    if mark in marks:
+        print("No change found.")
+        print(mark)
+        marks.pop(0)
+    else:
+        print("It changed!")
+
+while True:
+    session = requests.session()
+    login = requests.get("https://metameer.itslearning.com")
+    result = session.get(login)
+
+    tree = html.fromstring(result.text)
+    result = session.post(
+        login,
+        data = payload,
+        headers = dict(referer=login)
+    )
+
+    #page = requests.get("https://metameer.itslearning.com/index.aspx")
+    #soup = BeautifulSoup(page.content, 'html.parser')
+    #info = soup.find_all(class_="ptaVakGem")
+    #print (info)
